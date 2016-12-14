d576162f3e1d869c83e45e1faa7d92465f6df0c9
BAN Jun <banjun@gmail.com>
2016-12-14 11:09:31 +0900

    add forcedFuture {} and its tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,49 +2,46 @@
 
 import Quick
 import Nimble
+import BrightFutures
+import Result
 import JetToTheFuture
 
-class TableOfContentsSpec: QuickSpec {
+private func request(url: String) -> Future<HTTPURLResponse, DummyError> {
+    let p = Promise<HTTPURLResponse, DummyError>()
+    URLSession.shared.dataTask(with: URL(string: url)!) { data, response, error in
+        guard let response = response as? HTTPURLResponse else { return p.failure(DummyError(error: error)) }
+        p.success(response)
+    }.resume()
+    return p.future
+}
+
+class JetToTheFutureSpec: QuickSpec {
     override func spec() {
-        describe("these will fail") {
-
-            it("can do maths") {
-                expect(1) == 2
+        describe("forcedFuture") {
+            it("never deadlock") {
+                let result = forcedFuture {request(url: "https://www.apple.com/")}
+                let response = result.value
+                expect(response).notTo(beNil())
+                expect(response?.statusCode).to(equal(200))
             }
 
-            it("can read") {
-                expect("number") == "string"
+            it("correct async way") {
+                let result = request(url: "https://www.apple.com/")
+                expect(result.value).toNotEventually(beNil())
+                expect(result.value?.statusCode).toEventually(equal(200))
             }
 
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
-            
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    dispatch_async(dispatch_get_main_queue()) {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        NSThread.sleepForTimeInterval(0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
+            // replace xit() -> it() to test the test
+            xit("incorrect async way") {
+                let result = request(url: "https://www.apple.com/")
+                let response = result.value // incorrectly bind future value at the time
+                expect(response).notTo(beNil())
+                expect(response?.statusCode).toEventually(equal(200))
             }
         }
     }
+}
+
+struct DummyError: Error {
+    let error: Error?
 }
--- JetToTheFuture.podspec
+++ JetToTheFuture.podspec
@@ -1,42 +1,18 @@
-#
-# Be sure to run `pod lib lint JetToTheFuture.podspec' to ensure this is a
-# valid spec before submitting.
-#
-# Any lines starting with a # are optional, but their use is encouraged
-# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
-#
-
 Pod::Spec.new do |s|
   s.name             = 'JetToTheFuture'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of JetToTheFuture.'
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
-
+  s.summary          = 'safely `forced()` to get to the BrightFutures.Future values'
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+                       brings forced future block to safely `forced()` just on the main thread.
+                       its usecase is mainly for command-line and testcases
                        DESC
-
-  s.homepage         = 'https://github.com/<GITHUB_USERNAME>/JetToTheFuture'
-  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+  s.homepage         = 'https://github.com/banjun/JetToTheFuture'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'BAN Jun' => 'banjun@gmail.com' }
-  s.source           = { :git => 'https://github.com/<GITHUB_USERNAME>/JetToTheFuture.git', :tag => s.version.to_s }
-  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
-
-  s.ios.deployment_target = '8.0'
-
+  s.source           = { :git => 'https://github.com/banjun/JetToTheFuture.git', :tag => s.version.to_s }
+  s.social_media_url = 'https://twitter.com/banjun'
+  s.ios.deployment_target = '9.0'
+  s.osx.deployment_target = '10.11'
   s.source_files = 'JetToTheFuture/Classes/**/*'
-  
-  # s.resource_bundles = {
-  #   'JetToTheFuture' => ['JetToTheFuture/Assets/*.png']
-  # }
-
-  # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  s.dependency 'BrightFutures', '~> 5.0'
 end
